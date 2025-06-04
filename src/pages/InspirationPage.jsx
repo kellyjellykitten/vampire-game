98885094a113c0897cd42c23f039289ad2ba36ee
kelly <kellybackwards@gmail.com>
2025-06-04 16:29:23 -0400

    started inspo page

--- src/pages/InspirationPage.jsx
+++ src/pages/InspirationPage.jsx
@@ -1,5 +1,36 @@
 const InspirationPage = () => {
-    return <div>InspirationPage</div>;
+    return (
+        <div className="bg-gradient-to-b from-black to-gray-900 text-white min-h-screen p-4">
+            <div className="max-w-4xl mx-auto mt-12">
+                <h1 className="font-trade-winds text-3xl md:text-4xl font-bold mb-6 text-center">Inspiration/Help</h1>
+                <p className="text-base md:text-lg text-gray-200 leading-relaxed max-w-3xl mx-auto">Use this page to explore examples of the different aspects of character creation. You will find examples for your character&apos;s origin, mortal side characters, skills, resources, and conversion, in addition to completed character sheets. For further help, examples of experience entries and how they fit into memories have been included. A help modal is provided on each page of chracter creation to provide instructions. //// This page is designed to help you along the vampire creation process. While each page during  vampire creation will have a help modal, below are some general instructions and rules, along with examples for each aspect of the creation process are included as inspiration for your vampire. Examples of completed vampire characters are included at the bottom of the page.</p>
+            </div>
+            <div className="max-w-7xl mx-auto">
+                <div className="mt-6">                                          
+                    <h2>Jump to:</h2>
+                    <ul className="m-2">
+                        <li><a href="#origin">Origin</a></li>
+                        <li><a href="#sides">Side characters</a></li>
+                        <li>Skills</li>
+                        <li>Resources</li>
+                        <li>Conversion</li>
+                        <li>Memories</li>
+                        <li>Completed vampires</li>
+                    </ul>
+                </div>
+                <div className="mt-6">
+                    {/* Origin section */}
+                    <section id="origin">
+                        <h2 className="font-trade-winds text-3xl md:text-4xl px-6">Origin</h2>  
+                    </section>
+                    {/* Side character section */}
+                    <section id="sides">
+                        <h2>Side Characters</h2>
+                    </section>
+                </div>
+            </div>
+        </div>
+    )
 };
 
 export default InspirationPage;
--- to_do.md
+++ to_do.md
@@ -1,16 +1,16 @@
 # TO DO
 
 ## Styling
-- Navbar
-- Homepage cards
-- Darken vampire creation pages
+- Not found page
 - Thematic graphics
 - Sidebar character sheet positioning
+- Mobile styling for forms
+- Uniform buttons
 
 ## Content
 - Homepage text
 - Instructions text on every creation page
-- Text for help modal
+- Text for help modal (all shoudld include a link to inspiratino page that opens in new tab so it doesn't redirect user from creation)
 - More prompts
 - Adding additional entries to prompts in case same prompt is re-encountered
 - Inspiration page
@@ -21,9 +21,8 @@
 - Remove prompt number from local storage when user restarts game (i.e., when user goes back to start page then imports new character)
 ~~Prompt pages need accurate inputs/dropdowns for what the prompt dictates (i.e. selecting more than one skill in a dropdown)~~
 - Page showing changes needs to be either reworked, deleted, or fixed to show correct additions/strike-throughs
-- "Checking" a skill functionality (forgot what this means)
-- Adding a mark?
+- Way to see character creation progress (timeline & responses thus far)
 
 ## App-wide Considerations
-- A log or diary of the player's prompt answers
+- A log or diary of the player's prompt answers 
 - Possible log-in feature so user has an account & doesn't need to import & export their character every time ("Continue function")
